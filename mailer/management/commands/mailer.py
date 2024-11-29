d4b351a74fa6fe733c30ef27540d8f85f128c541
user.name <gmail@hdknr.com>
2024-11-29 11:15:35 +0900

    サーバー、テンプレート名の指定

--- mailer/management/commands/mailer.py
+++ mailer/management/commands/mailer.py
@@ -19,14 +19,22 @@ def main(ctx):
 
 
 @main.command()
-@click.argument("name")
-@click.argument("path")
+@click.argument("server_name")
+@click.argument("template_name")
+@click.argument("excel_path")
 @click.option("--to_fieild", "-t", default="to")
 @click.pass_context
-def send_batch(ctx, name, path, to_fieild):
+def send_batch(ctx, server_name, template_name, excel_path, to_fieild):
     """メールを送信する"""
-    server = models.MailServer.objects.first()
-    template = models.EmailTemplate.objects.first()
+    server = models.MailServer.objects.filter(name=server_name).first()
+    if not server:
+        print(f"{server_name}のサーバー設定がありません")
+        return
+    template = models.EmailTemplate.objects.filter(name=template_name).first()
+    if not template:
+        print(f"{template_name}のメールテンプレート設定ががありません")
+        return
+
     connection = server.get_connection()
 
     def send_row(row):
@@ -36,5 +44,5 @@ def send_batch(ctx, name, path, to_fieild):
             return
         template.send(to=[to], connection=connection, ctx=ctx)
 
-    data = pd.read_excel(path, dtype="str")
+    data = pd.read_excel(excel_path, dtype="str")
     data.apply(send_row, axis=1)
